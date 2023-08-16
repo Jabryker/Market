04015bf1301e3754c22374e40fd68a7a5c7fc38b
janelaxag <hellhole.gc@gmail.com>
2023-08-16 13:38:51 +0600

    add checkbox

--- src/templates/LoginPageTemplate/LoginPageTemplate.tsx
+++ src/templates/LoginPageTemplate/LoginPageTemplate.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import React, { FC, useState } from "react";
 import LoginFormOrganism from "../../components/organisms/LoginFormOrganism/LoginFormOrganism";
 import { ILoginData } from "../../controllers/interfaces/LoginData.interface";
 import AuthController from "../../controllers/AuthController";
@@ -6,15 +6,28 @@ import AuthController from "../../controllers/AuthController";
 import styles from "./LoginPageTemplate.module.scss";
 
 const LoginPageTemplate: FC = () => {
+  const [rememberMe, setRememberMe] = useState(false);
 
   const handleLoginSubmit = async (loginData: ILoginData) => {
-    await AuthController.login(loginData);
+    await AuthController.login(loginData, rememberMe);
+  };
+
+  const handleRememberMeChange = () => {
+    setRememberMe(!rememberMe);
   };
 
   return (
     <div className={styles.loginPage}>
       <h2 className={styles.loginPageTitle}>Войти</h2>
       <LoginFormOrganism onSubmit={handleLoginSubmit} />
+      <label>
+        <input
+          type="checkbox"
+          checked={rememberMe}
+          onChange={handleRememberMeChange}
+        />
+                Remember Me
+      </label>
     </div>
   );
 };
