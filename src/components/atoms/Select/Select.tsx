b19b10538a3f6f4adae4b2e3c18c5d649013425c
janelaxag <hellhole.gc@gmail.com>
2023-08-16 13:07:24 +0600

    select styles

--- src/components/atoms/Select/Select.tsx
+++ src/components/atoms/Select/Select.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import { ISelectProps } from "./Select.interface";
+import styles from "./Select.module.scss";
 
 const Select: FC<ISelectProps> = ({ label, name, options, register }) => {
   return (
-    <div>
-      <label>{label}</label>
-      <select name={name} {...register(name)}>
+    <div className={styles.selectСontainer}>
+      <label className={styles.selectLabel}>{label}</label>
+      <select className={styles.selectDropdown} name={name} {...register(name)}>
         {options.map((option, index) => (
           <option key={index} value={option}>
             {option}
