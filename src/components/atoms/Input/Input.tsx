bbb1d18691781276b35613f34b6febefde053e4c
janelaxag <hellhole.gc@gmail.com>
2023-08-16 13:07:11 +0600

    basic input style

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -1,17 +1,19 @@
 import {FC} from "react";
 import { IInputProps } from "./Input.interface";
+import styles from "./Input.module.scss";
 
 const Input: FC<IInputProps> = ({ type, name, label, placeholder, error, register }) => {
   return (
-    <div>
-      <label>{label}</label>
+    <div className={styles.Input}>
+      <label className={styles.Label} htmlFor={name}>{label}</label>
       <input
+        className={styles.InputField}
         type={type}
         name={name}
         placeholder={placeholder}
         {...register(name)}
       />
-      {error && <div>{error}</div>}
+      {error && <div className={styles.ErrorMessage} >{error}</div>}
     </div>
   );
 };
