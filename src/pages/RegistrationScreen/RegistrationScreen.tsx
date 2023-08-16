8593b84ff8cf0422151eac4007897f714dbcfd69
janelaxag <hellhole.gc@gmail.com>
2023-08-16 18:06:47 +0600

    add style and component registration screen

--- src/pages/RegistrationScreen/RegistrationScreen.tsx
+++ src/pages/RegistrationScreen/RegistrationScreen.tsx
@@ -1,12 +1,20 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import {Button} from "../../components/atoms/Button/Button";
+import styles from "./RegistrationScreen.module.scss";
 
 const RegistrationScreen: FC = () => {
   return (
-    <div>
-      <h2>Выберите тип регистрации:</h2>
-      <Link to="/registration/buyer">Регистрация как покупатель</Link>
-      <Link to="/registration/seller">Регистрация как продавец</Link>
+    <div className={styles.registrationScreenContainer}>
+      <h1 className={styles.registrationScreenTitle}>
+          Добро пожаловать в <span>MarketKG</span>
+      </h1>
+      <p className={styles.registrationScreenText}>В качестве кого вы хотите зарегистрироваться?</p>
+      <div className={styles.registrationScreenBtns}>
+        <Link to="/registration/buyer"><Button type="submit">Покупатель</Button></Link>
+        <span>или</span>
+        <Link to="/registration/seller"><Button type="submit">Продавец</Button></Link>
+      </div>
     </div>
   );
 };
