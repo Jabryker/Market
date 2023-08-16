744246c395b31a9411ff920714b74f0972eaa59d
janelaxag <hellhole.gc@gmail.com>
2023-08-16 19:55:42 +0600

    style buyer page add google

--- src/components/organisms/BuyerRegistrationFormOrganism/BuyerRegistrationFormOrganism.tsx
+++ src/components/organisms/BuyerRegistrationFormOrganism/BuyerRegistrationFormOrganism.tsx
@@ -1,15 +1,20 @@
 import {FC} from "react";
 import { useForm } from "react-hook-form";
+import GoogleLoginLogic from "../../../templates/GoogleLoginLogic/GoogleLoginLogic";
 import {Button} from "../../atoms/Button/Button";
 import BuyerRegistrationFormFields from "../../molecules/BuyerRegistrationFormFields/BuyerRegistrationFormFields";
 import {IBuyerRegistrationFormOrganismProps} from "./BuyerRegistrationFormOrganism.interface";
+
 
 const BuyerRegistrationFormOrganism: FC<IBuyerRegistrationFormOrganismProps> = ({ onSubmit }) => {
   const { register, handleSubmit, formState: { errors }  } = useForm();
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <BuyerRegistrationFormFields register={register} errors={errors} />
-      <Button type="submit">Submit</Button>
+      <div style={{ marginTop: "48px" }}>
+        <Button type="submit">Создать аккаунт</Button>
+        <GoogleLoginLogic>Создать аккаунт</GoogleLoginLogic>
+      </div>
     </form>
   );
 };
--- src/templates/BuyerRegistrationPageTemplate/BuyerRegistrationPageTemplate.tsx
+++ src/templates/BuyerRegistrationPageTemplate/BuyerRegistrationPageTemplate.tsx
@@ -1,7 +1,9 @@
 import {FC} from "react";
 import BuyerRegistrationFormOrganism from "../../components/organisms/BuyerRegistrationFormOrganism/BuyerRegistrationFormOrganism";
+import HelpText from "../../components/atoms/HelpText/HelpText";
 import AuthController from "../../controllers/AuthController";
 import {IRegisterData} from "../../controllers/interfaces/RegisterData.interface";
+import styles from "./BuyerRegistrationPageTemplate.module.scss";
 
 const BuyerRegistrationPageTemplate: FC = () => {
 
@@ -11,9 +13,10 @@ const BuyerRegistrationPageTemplate: FC = () => {
   };
 
   return (
-    <div>
-      <h2>Buyer Registration Page</h2>
+    <div className={styles.BuyerRegistrationPageTemplate}>
+      <h2 className={styles.BuyerRegistrationPageTemplateTitle}>Регистрация</h2>
       <BuyerRegistrationFormOrganism onSubmit={handleBuyerSubmit} />
+      <HelpText linkTo="/login" linkText="Войти">У вас есть аккаунт?</HelpText>
     </div>
   );
 };
