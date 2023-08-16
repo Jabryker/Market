91bf5946a98795edb0f8a5bfc5a4b90ce9716e82
janelaxag <hellhole.gc@gmail.com>
2023-08-16 17:51:01 +0600

    delete unused

--- src/templates/LoginPageTemplate/LoginPageTemplate.tsx
+++ src/templates/LoginPageTemplate/LoginPageTemplate.tsx
@@ -1,4 +1,6 @@
-import React, { FC, useState } from "react";
+import { FC } from "react";
+import HelpText from "../../components/atoms/HelpText/HelpText";
+import GoogleLoginLogic from "../GoogleLoginLogic/GoogleLoginLogic";
 import LoginFormOrganism from "../../components/organisms/LoginFormOrganism/LoginFormOrganism";
 import { ILoginData } from "../../controllers/interfaces/LoginData.interface";
 import AuthController from "../../controllers/AuthController";
@@ -6,28 +8,17 @@ import AuthController from "../../controllers/AuthController";
 import styles from "./LoginPageTemplate.module.scss";
 
 const LoginPageTemplate: FC = () => {
-  const [rememberMe, setRememberMe] = useState(false);
 
   const handleLoginSubmit = async (loginData: ILoginData) => {
-    await AuthController.login(loginData, rememberMe);
-  };
-
-  const handleRememberMeChange = () => {
-    setRememberMe(!rememberMe);
+    await AuthController.login(loginData);
   };
 
   return (
     <div className={styles.loginPage}>
       <h2 className={styles.loginPageTitle}>Войти</h2>
       <LoginFormOrganism onSubmit={handleLoginSubmit} />
-      <label>
-        <input
-          type="checkbox"
-          checked={rememberMe}
-          onChange={handleRememberMeChange}
-        />
-                Remember Me
-      </label>
+      <GoogleLoginLogic />
+      <HelpText linkTo="/registration" linkText="Создать аккаунт">Нет ещё аккаунта?</HelpText>
     </div>
   );
 };
