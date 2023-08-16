a4cdcfa241b1a514272a7563b2ae99a76bca082c
janelaxag <hellhole.gc@gmail.com>
2023-08-16 13:08:15 +0600

    add Login page styles

--- src/templates/LoginPageTemplate/LoginPageTemplate.tsx
+++ src/templates/LoginPageTemplate/LoginPageTemplate.tsx
@@ -1,17 +1,20 @@
 import { FC } from "react";
 import LoginFormOrganism from "../../components/organisms/LoginFormOrganism/LoginFormOrganism";
-import {ILoginData} from "../../controllers/interfaces/LoginData.interface";
+import { ILoginData } from "../../controllers/interfaces/LoginData.interface";
 import AuthController from "../../controllers/AuthController";
 
+import styles from "./LoginPageTemplate.module.scss";
+
 const LoginPageTemplate: FC = () => {
-  const handleLoginSubmit = (loginData: ILoginData) => {
-    AuthController.login(loginData);
+
+  const handleLoginSubmit = async (loginData: ILoginData) => {
+    await AuthController.login(loginData);
   };
 
   return (
-    <div>
-      <h2>Login page</h2>
-      <LoginFormOrganism onSubmit={handleLoginSubmit} /> 
+    <div className={styles.loginPage}>
+      <h2 className={styles.loginPageTitle}>Войти</h2>
+      <LoginFormOrganism onSubmit={handleLoginSubmit} />
     </div>
   );
 };
