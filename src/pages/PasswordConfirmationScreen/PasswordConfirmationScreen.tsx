036db79861899ff92bbecfa7ef22aeba64dad54e
Janelaxag <hellhole.gc@gmail.com>
2023-08-17 13:54:49 +0600

    add Password Confirmation

---
+++ src/pages/PasswordConfirmationScreen/PasswordConfirmationScreen.tsx
@@ -0,0 +1,22 @@
+import {FC} from "react";
+import HelpText from "../../components/atoms/HelpText/HelpText";
+import styles from "./PasswordConfirmationScreen.module.scss";
+
+const PasswordConfirmationScreen: FC = () => {
+  return (
+    <div className={styles.PassConfWrapper}>
+      <h2 className={styles.PassConfTitle}>Потвердите вашу почту</h2>
+      <p className={styles.PassConfSubTitle}>Письмо с подтверждением было отправлено на
+                example@gamil.com. Пожалуйста, перейдите по ссылке
+                внутри письма, чтобы начать использовать Marketplace.</p>
+      <div className={styles.PassConfComWrapper}>
+        <button>
+          <span>Отправить письмо ещё раз</span>
+        </button>
+        <HelpText linkTo="/help" linkText="Свяжитесь с нами">Есть жалобы? </HelpText>
+      </div>
+    </div>
+  );
+};
+
+export default PasswordConfirmationScreen;
