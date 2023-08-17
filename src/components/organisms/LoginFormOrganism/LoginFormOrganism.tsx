6bcbf6e9eacf50ae16188275ef2f931bf7327c55
Janelaxag <hellhole.gc@gmail.com>
2023-08-18 00:55:12 +0600

    add function for Login form

--- src/components/organisms/LoginFormOrganism/LoginFormOrganism.tsx
+++ src/components/organisms/LoginFormOrganism/LoginFormOrganism.tsx
@@ -1,16 +1,18 @@
-import { FC } from "react";
+import {FC} from "react";
 import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
 import {Button} from "../../atoms/Button/Button";
 import LoginFormFields from "../../molecules/LoginFormFields/LoginFormFields";
 import { ILoginFormOrganismProps } from "./LoginFormOrganism.interface";
 
 const LoginFormOrganism: FC<ILoginFormOrganismProps> = ({ onSubmit }) => {
   const { register, handleSubmit, formState: { errors }  } = useForm();
+  const { t } = useTranslation();
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <LoginFormFields register={register} errors={errors} />
-      <Button type="submit">Войти</Button>
+      <Button type="submit">{t("loginButton")}</Button>
     </form>
   );
 };
