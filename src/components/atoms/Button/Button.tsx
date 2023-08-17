4c93e3caeaef07f444cfeae4d672a83e6007866f
Janelaxag <hellhole.gc@gmail.com>
2023-08-18 00:54:31 +0600

    add button atom i18next

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,12 +1,14 @@
 import {FC} from "react";
+import {useTranslation} from "react-i18next";
 import {IButtonProps} from "./Button.interface";
 import styles from "./Button.module.scss";
 
 export const Button: FC<IButtonProps> = ({ children, type }) => {
+  const { t } = useTranslation();
   return (
     <button
       type={type}
       className={styles.formBtn}
-    > <span className={styles.buttonContent}>{children}</span></button>
+    > <span className={styles.buttonContent}>{t(children)}</span></button>
   );
 };
