ba083645863d1466d0403c76e4b736e5abdc55f3
Janelaxag <hellhole.gc@gmail.com>
2023-08-18 00:51:41 +0600

    wrapped i18n

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { I18nextProvider } from "react-i18next";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import i18n from "./i18n";
 import "./assets/styles/global.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <I18nextProvider i18n={i18n}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </I18nextProvider>
   </React.StrictMode>,
 );
