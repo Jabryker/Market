6e5db257a75df89283805c08011addbb4ecce0d6
Janelaxag <hellhole.gc@gmail.com>
2023-08-20 17:54:50 +0600

    delete i18n function

--- src/components/organisms/LoginFormOrganism/LoginFormOrganism.tsx
+++ src/components/organisms/LoginFormOrganism/LoginFormOrganism.tsx
@@ -1,19 +1,20 @@
-import {FC} from "react";
+import { FC } from "react";
 import { useForm } from "react-hook-form";
-import { useTranslation } from "react-i18next";
-import {Button} from "../../atoms/Button/Button";
+import { Button } from "../../atoms/Button/Button";
 import LoginFormFields from "../../molecules/LoginFormFields/LoginFormFields";
-import GoogleAuthOrgamism from "../GoogleAuthOrgamism/GoogleAuthOrgamism";
 import { ILoginFormOrganismProps } from "./LoginFormOrganism.interface";
 
 const LoginFormOrganism: FC<ILoginFormOrganismProps> = ({ onSubmit }) => {
-  const { register, handleSubmit, formState: { errors }  } = useForm();
-  const { t } = useTranslation();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <LoginFormFields register={register} errors={errors} />
-      <Button type="submit">{t("loginButton")}</Button>
+      <Button type="submit">{"Войти"}</Button>
     </form>
   );
 };
