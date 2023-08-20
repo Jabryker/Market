16fe8b69733958947f581fe2b6356acf054e8472
Janelaxag <hellhole.gc@gmail.com>
2023-08-20 18:48:07 +0600

    delete comments and styling subheader

--- src/components/organisms/SubHeaderOrganism/SubHeaderOrganism.tsx
+++ src/components/organisms/SubHeaderOrganism/SubHeaderOrganism.tsx
@@ -1,32 +1,32 @@
-import { FC } from "react";
+// SubHeaderOrganism.jsx
+
+import React, { FC } from "react";
 import ClockIcon from "../../../assets/images/header/time.svg";
 import WhatsAppIcon from "../../../assets/images/header/whatsapp.svg";
 import styles from "./SubHeaderOrganism.module.scss";
 
 const SubHeaderOrganism: FC = () => {
   return (
-    <div className={styles.SubHeaderOrganism}>
-      <span className={styles.LeftContent}>
-        <span className={`${styles.SubHeaderOrganismText} ${styles.Text}`}>
-          Welcome to worldwide Megamart!
-        </span>
-        <div className={styles.TimeAndWhatsApp}>
-          <img src={ClockIcon} alt="Clock Icon" />
-          <span className={styles.TimeRange}>08:00 - 19:00</span>
-          <img src={WhatsAppIcon} alt="WhatsApp Icon" />
-          <span className={styles.TimeRange}>+996707654499</span>
-          {/*<Dropdown className={styles.LanguageDropdown}>*/}
-          {/*  <Dropdown.Toggle variant="success" id="language-dropdown">*/}
-          {/*    {t("language")} /!* Текст кнопки языка из переводов *!/*/}
-          {/*  </Dropdown.Toggle>*/}
-          {/*  <Dropdown.Menu>*/}
-          {/*    <Dropdown.Item onClick={() => changeLanguage("ru")}>RU</Dropdown.Item>*/}
-          {/*    <Dropdown.Item onClick={() => changeLanguage("kg")}>KG</Dropdown.Item>*/}
-          {/*    <Dropdown.Item onClick={() => changeLanguage("en")}>US</Dropdown.Item>*/}
-          {/*  </Dropdown.Menu>*/}
-          {/*</Dropdown>*/}
+    <div className={`${styles.SubHeaderOrganism} container-fluid`}>
+      <div className="row align-items-center">
+        <div className="col-md-8 col-sm-12">
+          <span className={`${styles.SubHeaderOrganismText} ${styles.Text}`}>
+            Welcome to worldwide Megamart!
+          </span>
         </div>
-      </span>
+        <div className="col-md-4 col-sm-12">
+          <div className={`${styles.TimeAndWhatsApp} d-flex justify-content-end`}>
+            <div className={styles.TimeWrapper}>
+              <img src={ClockIcon} alt="Clock Icon" className={styles.Icon} />
+              <span className={styles.TimeRange}>08:00 - 19:00</span>
+            </div>
+            <div className={styles.WhatsAppWrapper}>
+              <img src={WhatsAppIcon} alt="WhatsApp Icon" className={styles.Icon} />
+              <span className={styles.TimeRange}>+996707654499</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
