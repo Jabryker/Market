bf52c03bfea29bc1e837de96a3290abc89010d84
Janelaxag <hellhole.gc@gmail.com>
2023-08-20 21:32:02 +0600

    partners data

---
+++ src/assets/data/partners.js
@@ -0,0 +1,14 @@
+const partners = [
+  {
+    id: 1,
+    logoUrl: "URL_TO_LOGO_1",
+    description: "Description of Partner 1",
+  },
+  {
+    id: 2,
+    logoUrl: "URL_TO_LOGO_2",
+    description: "Description of Partner 2",
+  },
+];
+
+export default partners;
