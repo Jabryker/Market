336d14cbbef8731ec3f8b7451ff90855c48a49e3
Janelaxag <hellhole.gc@gmail.com>
2023-08-20 21:31:53 +0600

    add partners

--- src/pages/MainScreen/MainPage.tsx
+++ src/pages/MainScreen/MainPage.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import HeaderOrganism from "../../components/organisms/HeaderOrganism/HeaderOrganism";
 import SubHeaderOrganism from "../../components/organisms/SubHeaderOrganism/SubHeaderOrganism";
+import PartnersOrganism from "../../components/organisms/PartnersOrganism/PartnersOrganism";
 import PreFooterOrgamism from "../../components/organisms/PreFooterOrgamism/PreFooterOrgamism";
 import FooterOrganism from "../../components/organisms/FooterOrganism/FooterOrganism";
 
@@ -9,6 +10,7 @@ const MainPage: FC = () => {
     <>
       <SubHeaderOrganism />
       <HeaderOrganism />
+      <PartnersOrganism />
       <PreFooterOrgamism />
       <FooterOrganism />
     </>
