ace3daa2e9b018eb38c398903e0b57899788f1f1
Janelaxag <hellhole.gc@gmail.com>
2023-08-21 23:40:15 +0600

    update and delete other comments

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-// import { FieldError } from "react-hook-form"; // Import FieldError type
 import { IInputProps } from "./Input.interface";
 import styles from "./Input.module.scss";
 
