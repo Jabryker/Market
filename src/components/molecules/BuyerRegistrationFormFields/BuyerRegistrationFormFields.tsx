c620b0cf452f53a3c1c4f4814f86fcce2246899a
Janelaxag <hellhole.gc@gmail.com>
2023-08-21 14:38:07 +0600

    add palceholder and validate

--- src/components/molecules/BuyerRegistrationFormFields/BuyerRegistrationFormFields.tsx
+++ src/components/molecules/BuyerRegistrationFormFields/BuyerRegistrationFormFields.tsx
@@ -1,36 +1,44 @@
-import {FC} from "react";
+import { FC } from "react";
 import Input from "../../atoms/Input/Input";
 import { IBuyerRegistrationFormProps } from "./BuyerRegistrationFormFields.interface";
+import styles from "./BuyerRegistrationFormFields.module.scss";
 
 const BuyerRegistrationFormFields: FC<IBuyerRegistrationFormProps> = ({ register, errors }) => {
-
   return (
     <>
-      <Input 
+      <Input
         label="Username"
         name="username"
-        type="text" 
-        register={register} 
+        type="text"
+        register={register}
         error={errors.username?.message}
       />
-      <Input 
-        label="Email" 
-        name="email" 
-        type="email" 
-        register={register} 
-        error={errors.email?.message} />
-      <Input 
-        label="Password" 
-        name="password" 
-        type="password" 
-        register={register} 
-        error={errors.password?.message} />
-      <Input 
-        label="Confirm Password" 
-        name="confirmPassword" 
-        type="password" 
-        register={register} 
-        error={errors.confirmPassword?.message} />
+      <Input
+        label="Email"
+        name="email"
+        type="email"
+        register={register}
+        error={errors.email?.message}
+      />
+      <Input
+        label="Password"
+        name="password"
+        type="password"
+        register={register}
+        error={errors.password?.message}
+      />
+      <Input
+        label="Confirm Password"
+        name="confirmPassword"
+        type="password"
+        register={register}
+        error={errors.confirmPassword?.message}
+      />
+      {errors.password &&
+        errors.confirmPassword &&
+        errors.password.message !== errors.confirmPassword.message && (
+        <div className={styles.ErrorMessage}>Passwords do not match</div>
+      )}
     </>
   );
 };
