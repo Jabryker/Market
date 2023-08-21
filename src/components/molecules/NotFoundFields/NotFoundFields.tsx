27f336d1785686c2fc4fee4b50120052fb7d62e0
Janelaxag <hellhole.gc@gmail.com>
2023-08-21 13:43:09 +0600

    Not Found molecules

---
+++ src/components/molecules/NotFoundFields/NotFoundFields.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../../atoms/Button/Button";
+import styles from "./NotFoundFields.module.scss";
+
+const NotFoundFields: FC = () => {
+  return (
+    <div className={styles.NotFoundFieldsWrapper}>
+      <h1 className={styles.NotFoundFieldsWrapperTitle}>404 Not Found</h1>
+      <span className={styles.NotFoundFieldsWrapperSubTitle}>
+        Your visited page not found. You may go home page.
+      </span>
+
+      <Link to="/">
+        <Button type="submit">Вернуться на главную</Button>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFoundFields;
