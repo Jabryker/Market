c7ae20706124cd7dfa8ed87fc010d1e0e5a5a5bb
Janelaxag <hellhole.gc@gmail.com>
2023-08-22 11:10:44 +0600

    update date in interface

--- src/components/atoms/CheckBox/CheckBox.tsx
+++ src/components/atoms/CheckBox/CheckBox.tsx
@@ -3,19 +3,13 @@ import { ICheckBox } from "./CheckBox.interface";
 import styles from "./CheckBox.module.scss";
 
 const CheckBox: FC<ICheckBox> = ({ name, label, register, error }) => {
-
   return (
     <div className={styles.checkboxContainer}>
       <label className={styles.checkboxLabel}>
-        <input
-          className={styles.checkboxInput}
-          type="checkbox"
-          name={name}
-          {...register(name)}
-        />
+        <input className={styles.checkboxInput} type="checkbox" {...register(name)} />
         {label}
       </label>
-      {error && <div>{error}</div>}
+      {error && <div>{error.message}</div>}
     </div>
   );
 };
