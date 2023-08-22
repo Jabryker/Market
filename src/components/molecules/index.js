2cf3b17bc2a282267a38350cf62f29bc3ecd91f1
Janelaxag <hellhole.gc@gmail.com>
2023-08-22 14:11:19 +0600

    index export

---
+++ src/components/molecules/index.js
@@ -0,0 +1,5 @@
+export { BuyerRegistrationFormFields } from "./BuyerRegistrationFormFields/BuyerRegistrationFormFields";
+export { LoginFormFields } from "./LoginFormFields/LoginFormFields";
+export { NotFoundFields } from "./NotFoundFields/NotFoundFields";
+export { PartnerCardFields } from "./PartnerCardFields/PartnerCardFirlds";
+export { SellerRegistrationFormFields } from "./SellerRegistrationFormFields/SellerRegistrationFormFields";
