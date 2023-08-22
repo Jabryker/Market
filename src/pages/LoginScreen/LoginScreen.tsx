cc5f22c36a92d1bb90c15cfc19adf7b42683311b
Janelaxag <hellhole.gc@gmail.com>
2023-08-22 14:18:22 +0600

    login page reexport

--- src/pages/LoginScreen/LoginScreen.tsx
+++ src/pages/LoginScreen/LoginScreen.tsx
@@ -1,12 +1,10 @@
 import { FC } from "react";
 import { LoginPageTemplate } from "../../templates/";
 
-const LoginScreen: FC = () => {
+export const LoginScreen: FC = () => {
   return (
     <div>
       <LoginPageTemplate />
     </div>
   );
 };
-
-export default LoginScreen;
