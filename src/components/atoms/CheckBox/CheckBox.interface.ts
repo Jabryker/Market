09db5cd505c5c62aa9a9bf6e18ce9c92f6a9cc46
Janelaxag <hellhole.gc@gmail.com>
2023-08-22 11:10:34 +0600

    update interface

--- src/components/atoms/CheckBox/CheckBox.interface.ts
+++ src/components/atoms/CheckBox/CheckBox.interface.ts
@@ -1,6 +1,8 @@
+import { UseFormRegister } from "react-hook-form";
+
 export interface ICheckBox {
-    name: string;
-    label?: string;
-    error?: any;
-    register: any;
+  name: string;
+  label?: string;
+  error?: any;
+  register: UseFormRegister<any>;
 }
