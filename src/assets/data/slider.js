365faba1c51a9a9fd1087621a91e73cea35a71d7
Janelaxag <hellhole.gc@gmail.com>
2023-08-23 11:33:45 +0600

    add silder data in assets

---
+++ src/assets/data/slider.js
@@ -0,0 +1,15 @@
+export const slideImages = [
+  {
+    id: 1,
+    url: require("../../../assets/images/slider/one.png"),
+    // caption: "Slide 1",
+  },
+  {
+    id: 2,
+    url: require("../../../assets/images/slider/one.png"),
+  },
+  {
+    id: 3,
+    url: require("../../../assets/images/slider/one.png"),
+  },
+];
