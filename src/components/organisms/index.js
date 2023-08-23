5d9937d24aa788622dc8b75dc1207518187819c5
Janelaxag <hellhole.gc@gmail.com>
2023-08-23 20:42:52 +0600

    delete prefooter

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -4,7 +4,6 @@ export { FooterOrganism } from "./FooterOrganism/FooterOrganism";
 export { GoogleAuthOrgamism } from "./GoogleAuthOrgamism/GoogleAuthOrgamism";
 export { HeaderOrganism } from "./HeaderOrganism/HeaderOrganism";
 export { LoginFormOrganism } from "./LoginFormOrganism/LoginFormOrganism";
-export { PreFooterOrgamism } from "./PreFooterOrgamism/PreFooterOrgamism";
 export { SellerRegistrationFormOrganism } from "./SellerRegistrationFormOrganism/SellerRegistrationFormOrganism";
 export { SliderOrganisms } from "./SliderOrganisms/SliderOrganisms";
 export { SubHeaderOrganism } from "./SubHeaderOrganism/SubHeaderOrganism";
