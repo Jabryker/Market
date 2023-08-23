f35be3ecd580622a5f40e0e7bb46a91b7caf98f8
Janelaxag <hellhole.gc@gmail.com>
2023-08-23 20:20:13 +0600

    add comment

--- src/utils/decodeAndLogJWT.ts
+++ src/utils/decodeAndLogJWT.ts
@@ -1,5 +1,11 @@
 import jwt_decode from "jwt-decode";
 
+/*
+ * Декодирует что нам присылает Google Auth
+ *
+ * @param {string} credential - JWT для декодирования
+ */
+
 export const decodeAndLogJWT = (credential: string) => {
   try {
     const decoded = jwt_decode(credential);
