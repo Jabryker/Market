5b6d7181134c366ecd1209eb3f3e42c71746a8a9
Janelaxag <hellhole.gc@gmail.com>
2023-08-30 19:40:16 +0600

    add get all product in product controller

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from "axios";
+import { IProduct } from "./interfaces/Product.interface";
 
 const baseApiUrl = process.env.REACT_APP_API_URL;
 
@@ -17,6 +18,19 @@ const ProductController = {
       const responseLogin: AxiosResponse = await axios.get(`${baseApiUrl}/api/v1/stores/products/`);
       console.log(responseLogin);
       return responseLogin.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+  getAllProduct: async () => {
+    try {
+      const response: AxiosResponse = await axios.get(`${baseApiUrl}/api/v1/stores/products/`);
+      console.log(response);
+
+      // Assuming your API response structure matches the data field of the response
+      const products: IProduct[] = response.data.results;
+
+      return products;
     } catch (error) {
       throw error;
     }
