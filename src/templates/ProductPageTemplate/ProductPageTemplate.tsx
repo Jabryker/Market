021890fbc168f21b84597aac96ee5bb4b4ad147c
Janelaxag <hellhole.gc@gmail.com>
2023-08-30 19:40:40 +0600

    product page

--- src/templates/ProductPageTemplate/ProductPageTemplate.tsx
+++ src/templates/ProductPageTemplate/ProductPageTemplate.tsx
@@ -1,7 +1,29 @@
-export const ProductPageTemplate = () => {
+import { FC, useEffect, useState } from "react";
+import { DiscountProductsMolecules } from "../../components/molecules";
+import ProductController from "../../controllers/ProductController";
+import { IProduct } from "../../controllers/interfaces/Product.interface";
+
+export const ProductPageTemplate: FC = () => {
+  const [allProduct, setAllProduct] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    try {
+      const fetchProducts = async () => {
+        const fetchedProducts: IProduct[] = await ProductController.getAllProduct();
+        setAllProduct(fetchedProducts);
+      };
+
+      fetchProducts();
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+
   return (
     <div>
-      ProductPageTemplate
+      <h2>Product Page</h2>
+      <DiscountProductsMolecules products={allProduct} />
     </div>
   );
 };
