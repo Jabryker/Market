2ec3db1b82cbaaaaed26af1766cf78e8aebb9598
Janelaxag <hellhole.gc@gmail.com>
2023-09-02 14:49:16 +0600

    add import for new atoms

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,11 +1,15 @@
+export { AddressFilter } from "./AddressFilter/AddressFilter";
 export { Button } from "./Button/Button";
 export { Category } from "./Category/Category";
+export { CategoryFilter } from "./CategoryFilter/CategoryFilter";
 export { CategorySelect } from "./CategorySelect/CategorySelect";
 export { CheckBox } from "./CheckBox/CheckBox";
 export { displayErrorToast } from "./displayErrorToast/displayErrorToast";
 export { displaySuccessToast } from "./displaySuccessToast/displaySuccessToast";
 export { HelpText } from "./HelpText/HelpText";
 export { Input } from "./Input/Input";
+export { NameFilter } from "./NameFilter/NameFilter";
+export { PriceRangeFilter } from "./PriceRangeFilter/PriceRangeFilter";
 export { ProductCard } from "./ProductCard/ProductCard";
 export { ProductSkeleton } from "./ProductSkeleton/ProductSkeleton";
 export { SkeletonCard } from "./SkeletonCard/SkeletonCard";
