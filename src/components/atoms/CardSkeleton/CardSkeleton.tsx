cf2ea506b9680fe74fceb4a5e549557f96ac2c53
Janelaxag <hellhole.gc@gmail.com>
2023-09-09 21:24:52 +0600

    skeleton

---
+++ src/components/atoms/CardSkeleton/CardSkeleton.tsx
@@ -0,0 +1,13 @@
+export const CardSkeleton = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="100%"
+      height="100%"
+      viewBox="0 0 400 400"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <rect x="0" y="0" rx="10" ry="10" width="100%" height="100%" fill="#E0E0E0" />
+    </svg>
+  );
+};
