8c8a3d778154daf7509e8e2ff4073b3bab1a239c
Janelaxag <hellhole.gc@gmail.com>
2023-09-09 21:05:20 +0600

    reexpoer contact item

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,3 +1,5 @@
-export { partners } from "./partners";
-export { navbar } from "./navbar";
+export { contactItems } from "./contactItems";
 export { footer } from "./footer";
+export { navbar } from "./navbar";
+export { partners } from "./partners";
+
