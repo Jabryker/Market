de8ba25db35f43bf25e0a72a29a55fc018b1e83c
janelaxag <hellhole.gc@gmail.com>
2023-09-14 15:02:54 +0600

    update ContactUsMolecule and InformationLine s and news styles

--- src/components/molecules/InformationLine/InformationLine.tsx
+++ src/components/molecules/InformationLine/InformationLine.tsx
@@ -1,14 +1,16 @@
-import {FC} from "react";
+import { FC } from "react";
 
 const InformationLine: FC = () => {
   return (
-    <div className="w-fullWidth bg-[#373737] text-white py-20 flex flex-col items-center justify-center">
-      <h2 className="text-center text-4xl font-semibold">
-        Хочешь доставку на дом?
+    <div
+      className="w-fullWidth bg-[#373737] text-white py-20 flex flex-col items-center justify-center rounded-tl-[50px] rounded-tr-[50px] shadow-md text-center"
+    >
+      <h2 className="text-4xl font-semibold">
+                Хочешь доставку на дом?
       </h2>
-      <p className="text-center text-2xl py-7" style={{ width: "45%" }}>
-        Бесплатная доставка от 20 000 сом по г. Бишкек, менее этой суммы
-        доставка от 1000 сом по г. Бишкек
+      <p className="text-2xl py-7" style={{ width: "45%" }}>
+                Бесплатная доставка от 20 000 сом по г. Бишкек, менее этой суммы
+                доставка от 1000 сом по г. Бишкек
       </p>
     </div>
   );
