fbff0043cb53856b0322c3dc908665c415d1b57e
janelaxag <hellhole.gc@gmail.com>
2023-09-15 14:27:51 +0600

    Update TitleText styles

--- src/components/atoms/TitleText/TitleText.tsx
+++ src/components/atoms/TitleText/TitleText.tsx
@@ -4,10 +4,9 @@ import { ITitleText } from "./TitleText.interface";
 export const TitleText: FC<ITitleText> = ({ children }) => {
   return (
     <div className="my-20">
-      <h1 className="title text-2xl md:text-4xl lg:text-5x1 text-center color-[#373737] ">
+      <h1 className="title text-4xl font-bold md:text-4xl lg:text-5x1 ml-6 color-[#373737] ">
         {children}
       </h1>
-      <div className="line mt-2 md:mt-4 lg:mt-6 bg-[#FB6D56] h-1 w-20 md:w-32 lg:w-64 mx-auto" />
     </div>
   );
 };
