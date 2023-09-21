26958e06240d027e9f031e04e60f19e513b0d08f
Janelaxag <hellhole.gc@gmail.com>
2023-09-22 00:45:46 +0600

    delete import unused

--- src/pages/ProductPages/ProductPages.tsx
+++ src/pages/ProductPages/ProductPages.tsx
@@ -1,12 +1,9 @@
-import { FooterOrganism, HeaderOrganism } from "../../components/organisms"
 import { ProductPageTemplate } from "../../templates"
 
 export const ProductPages = () => {
   return (
     <>
-      <HeaderOrganism />
       <ProductPageTemplate />
-      <FooterOrganism />
     </>
   )
 }
