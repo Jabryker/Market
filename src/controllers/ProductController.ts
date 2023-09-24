c5d2c865379e127ef43f4d50ce33a01fa5cf9b2d
Janelaxag <hellhole.gc@gmail.com>
2023-09-24 17:27:46 +0600

    delete not wirking function

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosResponse } from "axios";
-import { IProduct } from "./interfaces/Product.interface";
 
-const baseApiUrl = process.env.REACT_APP_API_URL;
+const baseApiUrl = process.env.REACT_APP_API_URL ?? "";
 
 const ProductController = {
   getProductSale: async () => {
@@ -18,32 +17,6 @@ const ProductController = {
       const responseLogin: AxiosResponse = await axios.get(`${baseApiUrl}/api/v1/stores/products/`);
       console.log(responseLogin);
       return responseLogin.data;
-    } catch (error) {
-      throw error;
-    }
-  },
-  getFilteredProducts: async (filters: {
-    category?: string;
-    name?: string;
-    minPrice?: number;
-    maxPrice?: number;
-    address?: string;
-  }) => {
-    try {
-      const response: AxiosResponse = await axios.get(`${baseApiUrl}/api/v1/stores/products/`, {
-        params: {
-          category: filters.category,
-          name: filters.name,
-          price__gte: filters.minPrice,
-          price__lte: filters.maxPrice,
-          address: filters.address,
-        },
-      });
-
-      // Assuming your API response structure matches the data field of the response
-      const products: IProduct[] = response.data.results;
-
-      return products;
     } catch (error) {
       throw error;
     }
