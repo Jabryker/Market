fcec80ecaed9538b3d1a36ecfb45351734a96967
Janelaxag <hellhole.gc@gmail.com>
2023-09-25 23:46:15 +0600

    delete SubheaderOrganism

--- src/components/organisms/NotFoundOrmanism/NotFoundOrmanism.tsx
+++ src/components/organisms/NotFoundOrmanism/NotFoundOrmanism.tsx
@@ -1,11 +1,9 @@
 import { FC } from "react"
-import { SubHeaderOrganism } from "../"
 import { NotFoundFields } from "../../molecules/"
 
 export const NotFoundOrmanism: FC = () => {
   return (
     <>
-      <SubHeaderOrganism />
       <NotFoundFields />
     </>
   )
