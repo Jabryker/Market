86f01e4fd5dff031b6b0597f5b7d5619b61e9e42
Myrzamuratov <yeees.nooo02@gmail.com>
2023-09-26 17:00:19 +0600

    middle profile finished

--- src/controllers/ProfileController.ts
+++ src/controllers/ProfileController.ts
@@ -15,37 +15,45 @@ export interface IUser {
 }
 
 export interface IUserProfile extends IUser {
-    certificate_number?: number;
-    INN?: number;
-    type?: string;
+  certificate_number?: number;
+  INN?: number;
+  type?: string;
 }
-
 
 export enum UserType {
   Buyer = "buyer",
   Seller = "seller",
 }
 
-
 const basicApi = process.env.REACT_APP_API_URL;
+const id = "2";
 
 export const ProfileController = {
-  fetchUserProfile: async (userType: UserType, userId: string): Promise<IUserProfile | IUser> => {
+  fetchUserProfile: async (
+    userType: UserType,
+    userId: string
+  ): Promise<IUserProfile | IUser> => {
     try {
       let endpoint = "";
       if (userType === UserType.Buyer) {
-        endpoint = `/api/v1/accounts/users/${userId}`;
+        endpoint = `/api/v1/accounts/users/${id}/`;
       } else {
-        endpoint = `/api/v1/accounts/sellers/${userId}`;
+        endpoint = `/api/v1/accounts/sellers/${id}/`;
       }
 
-      const response: AxiosResponse<IUserProfile | IUser> = await axios.get(`${basicApi}${endpoint}`);
+      const response: AxiosResponse<IUserProfile | IUser> = await axios.get(
+        `http://3.94.80.210/api/v1/accounts/users/${id}/`
+      );
       return response.data;
     } catch (error) {
       throw new Error("Error fetching user profile");
     }
   },
-  updateProfile: async (userType: UserType, userId: string, updatedUserData: IUserProfile): Promise<void> => {
+  updateProfile: async (
+    userType: UserType,
+    userId: string,
+    updatedUserData: IUserProfile
+  ): Promise<void> => {
     try {
       let endpoint = "";
       if (userType === UserType.Buyer) {
@@ -55,7 +63,6 @@ export const ProfileController = {
       }
 
       await axios.patch(`${basicApi}${endpoint}`, updatedUserData);
-
     } catch (error) {
       throw new Error("Error updating user profile");
     }
--- src/pages/ProfilePages/ProfilePages.tsx
+++ src/pages/ProfilePages/ProfilePages.tsx
@@ -1,8 +1,13 @@
 import { FC, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import {HeaderOrganism} from "../../components/organisms";
+import { HeaderOrganism } from "../../components/organisms";
 import { ProfileTemplate } from "../../templates/";
-import { ProfileController, IUserProfile, IUser, UserType } from "../../controllers/ProfileController";
+import {
+  ProfileController,
+  IUserProfile,
+  IUser,
+  UserType,
+} from "../../controllers/ProfileController";
 
 interface ProfileScreenProps {
   userType: string;
@@ -10,14 +15,20 @@ interface ProfileScreenProps {
 
 export const ProfilePages: FC<ProfileScreenProps> = ({ userType }) => {
   const { id } = useParams<{ id?: string }>();
-  const [profileData, setProfileData] = useState<IUserProfile | IUser | null>(null);
+  const [profileData, setProfileData] = useState<IUserProfile | IUser | null>(
+    null
+  );
 
   useEffect(() => {
     if (id) {
       const fetchUserProfile = async () => {
         try {
-          const userTypeValue = userType === "buyer" ? UserType.Buyer : UserType.Seller;
-          const data = await ProfileController.fetchUserProfile(userTypeValue, id);
+          const userTypeValue =
+            userType === "buyer" ? UserType.Buyer : UserType.Seller;
+          const data = await ProfileController.fetchUserProfile(
+            userTypeValue,
+            id
+          );
           setProfileData(data);
         } catch (error) {
           console.error("Error fetching profile:", error);
@@ -27,7 +38,6 @@ export const ProfilePages: FC<ProfileScreenProps> = ({ userType }) => {
       fetchUserProfile();
     }
   }, [userType, id]);
-
 
   return (
     <div>
