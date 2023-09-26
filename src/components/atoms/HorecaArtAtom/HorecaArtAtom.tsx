d3a06cebe4a14a752b6e9fe72a74cb2522885372
Janelaxag <hellhole.gc@gmail.com>
2023-09-26 15:48:25 +0600

    add border radius

--- src/components/atoms/HorecaArtAtom/HorecaArtAtom.tsx
+++ src/components/atoms/HorecaArtAtom/HorecaArtAtom.tsx
@@ -5,13 +5,13 @@ export const HorecaArtAtom: FC = () => {
   return (
     <div className="flex flex-row justify-between items-center m-20">
       <img src={HorecaArtAtomPhoto} alt="HorecaArtAtomPhoto" className="w-[800px] h-[400px]" />
-      <div className="rounded-lg border-3 border-[#EC9A1E] bg-[#EC9A1E] p-4 w-[900px] h-[400px] flex flex-col justify-center items-start">
+      <div className="rounded-3xl border-3 border-[#EC9A1E] bg-[#EC9A1E] p-4 w-[900px] h-[400px] flex flex-col justify-center items-start">
         <div className="text-white text-left ml-20">
           <h2 className="text-2xl font-semibold mb-4">HorecaArt</h2>
           <p className="text-lg mb-4">
                         Одна из цифровых площадок, <br /> где страсть к готовке и кухонное <br /> профессиональное оборудование.
           </p>
-          <button className="bg-white text-[#EC9A1E] border-r py-2 px-4 transition duration-300 hover:bg-[#EC9A1E] hover:text-white">
+          <button className="bg-white text-[#EC9A1E] border-r py-2 rounded-3xl px-4 transition duration-300 hover:bg-[#1965E1] hover:text-white">
                         Подробнее
           </button>
         </div>
